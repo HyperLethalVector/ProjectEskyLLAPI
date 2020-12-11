81906b1dc7c08f56fae17f1824d41ab65c1ab174
Damien Rompapas <hyperlethalvector92@gmail.com>
2020-12-11 14:59:46 +1030

    Updated common_header.h

--- src/Tracker/IntelT265/Tracker.cpp
+++ src/Tracker/IntelT265/Tracker.cpp
@@ -28,7 +28,7 @@
 #include <opencv2/calib3d.hpp>
 #include <opencv2/imgproc.hpp>
 #ifdef __linux__
-    #include <SerialPort.hpp>
+    #include <LinuxSerialPort.hpp>
     using namespace mn::CppLinuxSerial;
 #else
     #include "Serial.h"
